570fd8beb690c15567c597e5752299028b2e6582
David Fokkema <davidf@nikhef.nl>
2008-11-03 14:53:20 +0100

    Merged fixes on buffer.sql from windows-projects revision 68.

--- db_buffer/buffer.sql
+++ db_buffer/buffer.sql
@@ -1,17 +1,14 @@
 DROP DATABASE IF EXISTS `buffer`;
 CREATE DATABASE `buffer`;
-CONNECT `buffer`;
+USE `buffer`;
 
-DROP TABLE IF EXISTS `device`;
 CREATE TABLE `device` (
   `device_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `device_serial` VARCHAR(40) DEFAULT NULL,
   `sensor_password` varchar(20) DEFAULT NULL,
-  PRIMARY KEY  (`device_id`),
-  UNIQUE KEY `i_device_serial` (`device_serial`)
-) DEFAULT CHARSET=latin1;
+  PRIMARY KEY  (`device_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `message`;
 CREATE TABLE `message` (
   `message_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `device_id` INT(10) UNSIGNED DEFAULT NULL,
@@ -21,36 +18,37 @@ CREATE TABLE `message` (
   `timestamp` TIMESTAMP DEFAULT NOW(),
   `upload_status` INTEGER UNSIGNED NOT NULL DEFAULT '0',
   PRIMARY KEY  (`message_id`),
-  KEY `i_upload_status` (`device_id`,`message_status_id`,`upload_status`)
-) DEFAULT CHARSET=latin1;
+  KEY `i_upload_status` (`device_id`,`message_type_id`,`upload_status`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `message_status`;
 CREATE TABLE `message_status` (
   `message_status_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `description` varchar(40) DEFAULT NULL,
   PRIMARY KEY  (`message_status_id`)
-) DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `message_type`;
 CREATE TABLE `message_type` (
   `message_type_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `description` varchar(40) DEFAULT NULL,
   PRIMARY KEY  (`message_type_id`)
-) DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `settings`;
 CREATE TABLE `settings` (
   `code` VARCHAR(3) NOT NULL,
   `description` VARCHAR(40) NULL,
   `value` TEXT NULL,
   PRIMARY KEY(`code`)
-);
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 
 -- Adding users 
 -- First make sure the user exists before dropping it. Braindamage: there
--- is no drop user if exists statemtent.
-GRANT USAGE ON *.* TO `buffer`@`%`;
+-- is no DROP USER IF EXISTS statement.
+GRANT USAGE ON *.* TO `buffer`@`%` IDENTIFIED BY 'foo';
+GRANT USAGE ON *.* TO `buffer`@`localhost` IDENTIFIED BY 'foo';
 DROP USER `buffer`@`%`;
+DROP USER `buffer`@`localhost`;
 CREATE USER `buffer`@`%` IDENTIFIED BY 'PLACEHOLDER';
-GRANT ALL ON buffer.* TO buffer;
+CREATE USER `buffer`@`localhost` IDENTIFIED BY 'PLACEHOLDER';
+GRANT ALL ON `buffer`.* TO `buffer`@`%`;
+GRANT ALL ON `buffer`.* TO `buffer`@`localhost`;
