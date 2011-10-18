f36f0966a2aea654175a7ea01b38e396413e5b05
Arne de Laat <adelaat@nikhef.nl>
2011-10-18 11:40:33 +0200

    Fixed some spacing/indentation.

--- user/hsmonitor/src/NagiosPush.py
+++ user/hsmonitor/src/NagiosPush.py
@@ -5,19 +5,19 @@ from hslog import log
 
 class NagiosPush:
     def __init__(self, config):
-    self.host = config["host"]
-    self.port = int(config["port"])
-    self.machine_name = config["machine_name"]
+        self.host = config["host"]
+        self.port = int(config["port"])
+        self.machine_name = config["machine_name"]
 
     def sendToNagios(self, nagiosResult):
         reportMessage = {}
-        reportMessage['reportCode'] = nagiosResult.status_code          # Report code to send to Nagios
-        reportMessage['textMessage']= nagiosResult.description          # Message string to send to Nagios
-        reportMessage['send_nscaPath'] = "../data/send_nsca_win32/"     # Path to the send_nsca.exe
-        reportMessage['nagiosServer'] = self.host                       # Nagios server Ip address
-        reportMessage['serverPort'] = self.port                         # server port
-        reportMessage['hostComputer'] = self.machine_name               # On nagios server
-        reportMessage['serviceName'] = nagiosResult.serviceName         # Service name on Nagios server
+        reportMessage['reportCode'] = nagiosResult.status_code      # Report code to send to Nagios
+        reportMessage['textMessage']= nagiosResult.description      # Message string to send to Nagios
+        reportMessage['send_nscaPath'] = "../data/send_nsca_win32/" # Path to the send_nsca.exe
+        reportMessage['nagiosServer'] = self.host                   # Nagios server Ip address
+        reportMessage['serverPort'] = self.port                     # server port
+        reportMessage['hostComputer'] = self.machine_name           # On nagios server
+        reportMessage['serviceName'] = nagiosResult.serviceName     # Service name on Nagios server
 
         send_nsca_command = "echo %s,%s,%s,%s | %ssend_nsca -H %s -p %d -c %ssend_nsca.cfg -d ," % \
                                                  (reportMessage['hostComputer'],reportMessage['serviceName'], \
@@ -26,8 +26,8 @@ class NagiosPush:
                                                   reportMessage['serverPort'], reportMessage['send_nscaPath'])
         v = subprocess.Popen(send_nsca_command, shell=True, stdout=subprocess.PIPE)
         v.wait()
-    res = v.communicate()[0]
-    log ("Check: %s: Status code: %i, Status description: %s \n\t %s" % (nagiosResult.serviceName, nagiosResult.status_code, nagiosResult.description, res))
+        res = v.communicate()[0]
+        log ("Check: %s: Status code: %i, Status description: %s \n\t %s" % (nagiosResult.serviceName, nagiosResult.status_code, nagiosResult.description, res))
 
 
 
