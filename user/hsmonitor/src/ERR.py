a35cf9029e386dd91b4216aed7978a5615c63e13
Arne de Laat <adelaat@nikhef.nl>
2011-10-20 12:01:04 +0200

    Fixed docstrings, removed 'End of' lines and reflowed text.

--- user/hsmonitor/src/ERR.py
+++ user/hsmonitor/src/ERR.py
@@ -1,7 +1,4 @@
-"""
-    Process HiSPARC messages from a buffer.
-    This module processes the ERR event message
-"""
+"""Process the ERR event message from a buffer."""
 
 __author__="thevinh"
 __date__ ="$17-sep-2009"
@@ -11,31 +8,27 @@ from HiSparc2Event import HiSparc2Event
 
 class ERR(HiSparc2Event):
     def __init__(self, message):
-        """ Initialization
-            Proceed to unpack the message.
-        """
+        """Proceed to unpack the message."""
         # invoke constructor of parent class
         HiSparc2Event.__init__(self, message)
 
-    #--------------------------End of __init__--------------------------#
-
     def unpackMessage(self):
-        """ Unpack a buffer error message
-            This routine unpacks the error messages written to the buffer by
-            the LabVIEW DAQ software
+        """Unpack a buffer error message.
+        
+        This routine unpacks the error messages written to the buffer by the
+        LabVIEW DAQ software.
+            
         """
 
         # Initialize sequential reading mode
         self.unpackSeqMessage()
 
-        self.version, self.database_id, \
-        gps_second, gps_minute, gps_hour, gps_day, gps_month, gps_year = \
-        self.unpackSeqMessage('>2B5BH')
+        self.version, self.database_id, gps_second, gps_minute, gps_hour, \
+        gps_day, gps_month, gps_year = self.unpackSeqMessage('>2B5BH')
 
         self.datetime = datetime.datetime(gps_year, gps_month, gps_day,
                                           gps_hour, gps_minute, gps_second)
+        
         self.nanoseconds = 0
 
         self.error_message, = self.unpackSeqMessage('LVstring')
-
-    #--------------------------End of unpackMessage--------------------------#    
