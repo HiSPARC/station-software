91fff01ddb71e337aff87126ec9de437b7b3b5a6
Arne de Laat <adelaat@nikhef.nl>
2011-10-20 12:01:54 +0200

    Fixed docstring and removed 'End of' line

--- user/hsmonitor/src/Event.py
+++ user/hsmonitor/src/Event.py
@@ -1,12 +1,10 @@
-"""
-    This module creates different types of Events that are specified by the subclasses
-"""
+"""This module creates types of Events that are specified by the subclasses."""
 
 __author__="thevinh"
 __date__ ="$16-sep-2009"
 
 class Event():
-    # the instantiation operation
+    # The instantiation operation
     def __init__(self):
         # init variables here if needed
         self.datetime = 0
@@ -14,8 +12,6 @@ class Event():
         self.data = 0
         self.nanoseconds = 0
         self.export_values = 0
-
-    #--------------------------End of __init__--------------------------#
 
     def getEventData(self):
         pass
