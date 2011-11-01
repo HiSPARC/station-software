6e4cc1934bc08f93ff391678569c91a89d00b46a
Arne de Laat <adelaat@nikhef.nl>
2011-11-01 14:40:30 +0100

    Fixed bad spacing.

--- user/startstop/StopUserMode.py
+++ user/startstop/StopUserMode.py
@@ -12,15 +12,15 @@ def stop():
 		log('Stopping LabView...')
 		labViewHandler=StartStop()
 		labViewHandler.exeName='hisparcdaq.exe'
-		labViewHandler.currentDirectory="%s\\user\\hisparcdaq" %path
-		#labViewHandler.command="%s\\user\\hisparcdaq\\hisparcdaq.exe" %path
+		labViewHandler.currentDirectory="%s\\user\\hisparcdaq" % path
+		#labViewHandler.command="%s\\user\\hisparcdaq\\hisparcdaq.exe" % path
 		resLabView=labViewHandler.stopProcess()
 		if resLabView==0:
 			log('Status:running')
 		elif resLabView==1:
 			log('Status:stopped')
 		else:
-			log ('An exception was generated!')
+			log('An exception was generated!')
 	except:
 		log('An exception was generated while stopping LabView!')
 
@@ -29,14 +29,14 @@ def stop():
 		log('Stopping LabView Weather...')
 		labViewHandler=StartStop()
 		labViewHandler.exeName='hisparcweather.exe'
-		labViewHandler.currentDirectory="%s\\user\\hisparcweather" %path
+		labViewHandler.currentDirectory="%s\\user\\hisparcweather" % path
 		resLabView=labViewHandler.stopProcess()
 		if resLabView==0:
 			log('Status:running')
 		elif resLabView==1:
 			log('Status:stopped')
 		else:
-			log ('An exception was generated!')
+			log('An exception was generated!')
 	except:
 		log('An exception was generated while stopping LabView Weather!')
 	
@@ -48,14 +48,14 @@ def stop():
 		mySqlHandler.ShowWindow=win32con.SW_HIDE
 		#mySqlHandler.command="cmd /c mysqld.exe"
 		#FIXME: find command to shut down mysql nicely!
-		mySqlHandler.currentDirectory="%s\\user\\mysql\\bin" %path
+		mySqlHandler.currentDirectory="%s\\user\\mysql\\bin" % path
 		resMySql=mySqlHandler.stopProcess()
 		if resMySql==0:
 			log('Status:running')
 		elif resMySql==1:
 			log('Status:stopped')
 		else:
-			log ('An exception was generated!')
+			log('An exception was generated!')
 	except:
 		log('An exception was generated while stopping MySQL!')
 	
@@ -65,7 +65,7 @@ def stop():
 		hsMonitorHandler=CMDStartStop()
 		hsMonitorHandler.exeName='python.exe'
 		hsMonitorHandler.title='HISPARC MONITOR: hsmonitor'
-		#hsMonitorHandler.currentDirectory="%s\\user\\hsmonitor" %path
+		#hsMonitorHandler.currentDirectory="%s\\user\\hsmonitor" % path
 		#hsMonitorHandler.command="cmd.exe /c %s\\user\\python\\python.exe hsmonitor.py" % path
 		resHSMonitor=hsMonitorHandler.stopProcess()
 		if resHSMonitor==0:
@@ -73,7 +73,7 @@ def stop():
 		elif resHSMonitor==1:
 			log('Status:stopped')
 		else:
-			log ('An exception was generated')
+			log('An exception was generated')
 	except:
 		log('An exception was generated while stopping HSMonitor!')
 	
@@ -84,7 +84,7 @@ def stop():
 		updaterHandler=CMDStartStop()
 		updaterHandler.exeName='python.exe'
 		updaterHandler.title='HISPARC Updater: updater'
-		#hsMonitorHandler.currentDirectory="%s\\user\\hsmonitor" %path
+		#hsMonitorHandler.currentDirectory="%s\\user\\hsmonitor" % path
 		#hsMonitorHandler.command="cmd.exe /c %s\\user\\python\\python.exe hsmonitor.py" % path
 		resUpdater=updaterHandler.stopProcess()
 		if resUpdater==0:
@@ -92,7 +92,7 @@ def stop():
 		elif resUpdater==1:
 			log('Status:stopped')
 		else:
-			log ('An exception was generated')
+			log('An exception was generated')
 	except:
 		log('An exception was generated while stopping the Updater!')
 		
