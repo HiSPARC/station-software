5c9a33dfcdeb53a79962ba103f1388539139102f
Arne de Laat <adelaat@nikhef.nl>
2011-11-15 15:43:58 +0100

    Tabs to spaces, reflowed text and made more consistent with rest

--- user/startstop/StartAdminMode.py
+++ user/startstop/StartAdminMode.py
@@ -1,59 +1,56 @@
 from startStop import *
 from hslog import *
-import os
 
-path='%s:' %os.getenv("HISPARC_DRIVE")
 def start():
+    setLogMode(MODE_BOTH)
+    log('\nStarting Admin-Mode applications...')
 
-	setLogMode(MODE_BOTH)
-	
-	log('\nStarting Admin-Mode applications...')
-	try:
-		#start tightVNC
-		log('Starting TightVNC service...')
-		tightVNCHandler=StartStop()
-		tightVNCHandler.serviceName='tvnserver'
-		resTightVNC=tightVNCHandler.startService()
-		if resTightVNC==0:
-			log('Status:running')
-		elif resTightVNC==1:
-			log('Status:stopped')
-		else:
-			log('The service was not found!')
-	except:
-		log('An exception was generated while starting TightVNC:'  + str(sys.exc_info()[1]))
-	
-		
-	try:
-		#start Nagios Service
-		log('Starting Nagios Service')
-		nagiosServHandler=StartStop()
-		nagiosServHandler.serviceName= "NSClientpp"
-		resNagios=nagiosServHandler.startService()
-		if resNagios==0:
-			log('Status:running')
-		elif resNagios==1:
-			log('Status:stopped')
-		else:
-			log('The service was not found!')
-	except:
-		log('An exception was generated while starting the Nagios Service:' + str(sys.exc_info()[1]))
+    try:
+        #start tightVNC
+        log('Starting TightVNC service...')
+        tightVNCHandler = StartStop()
+        tightVNCHandler.serviceName = 'tvnserver'
+        resTightVNC = tightVNCHandler.startService()
+        if resTightVNC == 0:
+            log('Status:running')
+        elif resTightVNC == 1:
+            log('Status:stopped')
+        else:
+            log('The service was not found!')
+    except:
+        log('An exception was generated while starting TightVNC:' +
+            str(sys.exc_info()[1]))
 
-	try:
-		#start OpenVpn Service
-		log('Starting OpenVpn Service')
-		openVpnServHandler=StartStop()
-		openVpnServHandler.serviceName= "OpenVPNService"
-		resOpenVpn=openVpnServHandler.startService()
-		if resOpenVpn==0:
-			log('Status:running')
-		elif resOpenVpn==1:
-			log('Status:stopped')
-		else:
-			log('The service was not found!')
-	except:
-		log('An exception was generated while starting the OpenVPN Service:' + str(sys.exc_info()[1]))
+    try:
+        #start Nagios Service
+        log('Starting Nagios Service')
+        nagiosServHandler = StartStop()
+        nagiosServHandler.serviceName = "NSClientpp"
+        resNagios = nagiosServHandler.startService()
+        if resNagios == 0:
+            log('Status:running')
+        elif resNagios == 1:
+            log('Status:stopped')
+        else:
+            log('The service was not found!')
+    except:
+        log('An exception was generated while starting the Nagios Service:' +
+            str(sys.exc_info()[1]))
 
-
+    try:
+        #start OpenVpn Service
+        log('Starting OpenVpn Service')
+        openVpnServHandler = StartStop()
+        openVpnServHandler.serviceName = "OpenVPNService"
+        resOpenVpn = openVpnServHandler.startService()
+        if resOpenVpn == 0:
+            log('Status:running')
+        elif resOpenVpn == 1:
+            log('Status:stopped')
+        else:
+            log('The service was not found!')
+    except:
+        log('An exception was generated while starting the OpenVPN Service:' +
+            str(sys.exc_info()[1]))
 
 start()
