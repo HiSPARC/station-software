970d2cb87a37d04db4a79641804731a8ab440732
Arne de Laat <adelaat@nikhef.nl>
2011-11-16 14:06:23 +0100

    Added timeout to subprocess and fied some capitalization

--- user/hsmonitor/src/NagiosPush.py
+++ user/hsmonitor/src/NagiosPush.py
@@ -2,7 +2,13 @@ import subprocess
 from Check import *
 import sys
 from hslog import log
+import time
 
+TIMEOUT = 10
+
+class TimeoutException(Exception):
+    pass
+       
 class NagiosPush:
     def __init__(self, config):
         self.host = config["host"]
@@ -14,8 +20,8 @@ class NagiosPush:
         reportMessage['reportCode'] = nagiosResult.status_code      # Report code to send to Nagios
         reportMessage['textMessage'] = nagiosResult.description     # Message string to send to Nagios
         reportMessage['send_nscaPath'] = "../data/send_nsca_win32/" # Path to the send_nsca.exe
-        reportMessage['nagiosServer'] = self.host                   # Nagios server Ip address
-        reportMessage['serverPort'] = self.port                     # server port
+        reportMessage['nagiosServer'] = self.host                   # Nagios server IP address
+        reportMessage['serverPort'] = self.port                     # Server port
         reportMessage['hostComputer'] = self.machine_name           # On nagios server
         reportMessage['serviceName'] = nagiosResult.serviceName     # Service name on Nagios server
 
@@ -25,8 +31,18 @@ class NagiosPush:
                             reportMessage['send_nscaPath'], reportMessage['nagiosServer'],
                             reportMessage['serverPort'], reportMessage['send_nscaPath'])
         v = subprocess.Popen(send_nsca_command, shell=True, stdout=subprocess.PIPE)
-        v.wait()
-        res = v.communicate()[0]
+        t0 = time.time()
+        try:
+            while v.poll() is None:
+                elapsed_time = time.time() - t0
+                if elapsed_time >= TIMEOUT:
+                    raise TimeoutException("Process won't quit!")
+                time.sleep(1)
+        except TimeoutException:
+            v.kill()
+            res = "send_nsca_command failed"
+        else:
+            res = v.communicate()[0]
         log("Check %s: Status code: %i, Status description: %s.\n\t %s." %
             (nagiosResult.serviceName, nagiosResult.status_code,
              nagiosResult.description, res))
