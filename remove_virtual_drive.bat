d894893475bb0b39ea99a087cec8c990c5f8beb3
Arne de Laat <adelaat@nikhef.nl>
2012-01-04 14:41:22 +0100

    No longer used! Virtual Drive is not created in first place. To be removed soon.

--- remove_virtual_drive.bat
+++ remove_virtual_drive.bat
@@ -1,13 +1,8 @@
-@echo off
+:: @echo off
 
-:main
+:: :main
 
-  :: sets %HISPARC_DRIVE%, just in case this batch file is not called from the hisparc_cmd window
-  call %~dps0\persistent\configuration\startup_settings.bat
+  :: sets %HISPARC_ROOT%, just in case this batch file is not called from the hisparc_cmd window
+  :: call %~dps0\persistent\configuration\startup_settings.bat
 
-
-  echo removing drive %HISPARC_DRIVE%..
-  subst /D %HISPARC_DRIVE%:
-  if not errorlevel 1 echo success!
-
-  pause
+  :: pause
