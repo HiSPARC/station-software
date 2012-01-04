c6f3e2424b9b426aa42fcf23e16efeb8fd590cae
Arne de Laat <adelaat@nikhef.nl>
2012-01-04 14:37:43 +0100

    Use HISPARC_ROOT instead of HISPARC_DRIVE

--- hisparc_cmd.bat
+++ hisparc_cmd.bat
@@ -3,8 +3,8 @@
 :: set up environment variables and create virtual drive
 call "%~dps0"\user\startstop\setenv.bat
 
-:: change to virtual drive
-%HISPARC_DRIVE%:
+:: change to HiSPARC root path
+%HISPARC_ROOT%:
 
 :: start command interface
 cmd /k "title hisparc_cmd"
