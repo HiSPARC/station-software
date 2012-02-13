c49b8ba320a8360bc07bd321e5f1ea3788ac0771
Arne de Laat <adelaat@nikhef.nl>
2012-02-13 14:25:20 +0100

    Correctly change to HISPARC_ROOT

--- hisparc_cmd.bat
+++ hisparc_cmd.bat
@@ -1,10 +1,10 @@
 @echo off
 
-:: set up environment variables and create virtual drive
+:: set up environment variables
 call "%~dps0"\user\startstop\setenv.bat
 
 :: change to HiSPARC root path
-%HISPARC_ROOT%:
+cd /d %HISPARC_ROOT%
 
 :: start command interface
 cmd /k "title hisparc_cmd"
