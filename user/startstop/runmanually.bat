a7df92b403dbcd6918d83b9545afee26d6b84da3
Robert Hart <robert@nikhef.nl>
2012-03-20 16:08:49 +0100

    \user\startstop: setenv.bat & runmanually.bat: comment lines updated

--- user/startstop/runmanually.bat
+++ user/startstop/runmanually.bat
@@ -1,34 +1,36 @@
 @echo off
+
+::
+:: runmanually.bat ---
+::
+:: Modified Feb 2012; No virtual drive (HISPARC_DRIVE) anymore!! (RH)
+::
 :: Batch file to help running applications in the context of the HiSPARC
-:: virtual drive. Sets the environment variables, creates the virtual drive
-:: if it does not yet exists, and launches the specified COMMAND in
+:: system. Sets the environment variables and launches the specified COMMAND in
 :: the specified PATH. If COMMAND ends with ".py", the python interpreter
-:: is automatically used. Otherwise, COMMAND is simply executed as if it were
-:: typed on the command line.
+:: (as part of the HiSPARC distribution)is automatically used.
+:: Otherwise, COMMAND is simply executed as if it were typed on the command line.
 ::
 :: Note: the name of this file is slightly incorrect: it has nothing to do
 :: with whether a program is run manually or automatically - it just runs the
 :: program.
 ::
-:: invoke as 
-::
+:: invoke as:
 ::   @call "%~dp0..\startstop\runmanually.bat" PATH COMMAND [args]
 ::
-:: examples:
-::
+:: example:
 ::   @call "%~dp0..\startstop\runmanually.bat" \user\hsmonitor hsmonitor.py
-
+::
 
 :main
 
   set HISPARC_RUNMANUAL_PATH=%~1
   set HISPARC_RUNMANUAL_CMD=%~2
   
-  :: call setenv and create virtual drive
+  :: call setenv to set the environment variables and path
   call "%~dp0setenv.bat"
   
-  :: go to HiSPARC root and application directory
-  
+  :: go to application directory
   pushd %HISPARC_ROOT%%HISPARC_RUNMANUAL_PATH%
 
   if "%HISPARC_RUNMANUAL_CMD:~-3%"==".py" (
@@ -39,33 +41,19 @@
   
   :: actually execute application
   %HISPARC_RUNMANUAL_DO%
-  
-  :: if an error occured, wait for keypress, else close.
-  :: DF: removed because it breaks nagios checks
-  ::if errorlevel 1 pause
 
   :: switch back to old drive
   popd
-  
   goto :EOF
 
-
 :python
-
   shift /1
   shift /1
-
-  set HISPARC_RUNMANUAL_DO=\user\python\python.exe %HISPARC_RUNMANUAL_CMD% %1 %2 %3 %4 %5 %6 %7 %8 %9 
-
-goto :EOF
-
+  set HISPARC_RUNMANUAL_DO=python.exe %HISPARC_RUNMANUAL_CMD% %1 %2 %3 %4 %5 %6 %7 %8 %9 
+  goto :EOF
 
 :exe
-
   shift /1
   shift /1
-  
   set HISPARC_RUNMANUAL_DO=%HISPARC_RUNMANUAL_CMD% %1 %2 %3 %4 %5 %6 %7 %8 %9
-
-goto :EOF
-
+  goto :EOF
--- user/startstop/setenv.bat
+++ user/startstop/setenv.bat
@@ -1,44 +1,30 @@
 @echo off
 
+::
+:: setenv.bat ---
+:: Sets HISPARC_ROOT, home of HiSPARC.
+::
+:: Modified Feb 2012; No HISPARC_DRIVE anymore!! (RH)
+::
+
 :main
 
-  :: Store current directory and change to directory of this batch file
+  :: store current directory and change to directory of this batch file
   pushd "%~dp0"
 
-  :: Go two directories up, to the root of the (user) directory
+  :: go two directories up, to the root of HiSPARC
   cd ..\..
-
-  :: Global environment variables, that we assume everybody has
   set HISPARC_ROOT=%CD%
+  :: echo setenv.bat: HISPARC_ROOT=%HISPARC_ROOT%
 
-  :: sets %HISPARC_DRIVE%
-  :: set HISPARC_DRIVE=Z
-
-  :: Append semicolon to %PYTHONPATH%, if and only if it already exists,
-  :: as a separator for what is added next
+  :: append semicolon to %PYTHONPATH% if and only if it already exists
   if not "%PYTHONPATH%"=="" set PYTHONPATH=%PYTHONPATH%;
-
-  :: ADL: Change all HISPARC_DRIVE references to the current working dir
-  ::      (or to the path stored in the Windows registry)
   set PYTHONPATH=%PYTHONPATH%%HISPARC_ROOT%\user\pythonshared
+  :: echo setenv.bat: PYTHONPATH=%PYTHONPATH%
 
-  :: (disabled) Create virtual drive
-  :: subst %HISPARC_DRIVE%: "%HISPARC_ROOT%" > NUL
+  :: put HiSPARC's python in front of path
+  set path=%HISPARC_ROOT%\user\python;%path%
+  :: echo setenv.bat: path=%path%
 
-  :: The python-windows bindings assume that the files in the
-  :: python\Lib\site-packages\pywin32_system32 directory were copied to
-  :: system32. We don not want that because that would be mean that whenever
-  :: this changes (for example, possibly with a python version upgrade), the
-  :: admin-updater would need to be run.
-  ::
-  :: Instead, we add the directory containing these DLL files to the PATH before
-  :: starting any application. because this startup script calls the start
-  :: scripts, everything we run inherits this environment.
-  :: set path=%path%;%HISPARC_DRIVE%:\user\python\Lib\site-packages\pywin32_system32
-  ::
-  :: DF: UNFORTUNATELY, this does not work anymore. I have copied around some
-  ::     dll's and manifests and we now add the python directory to the path.
-  set path=%path%;%HISPARC_ROOT%\user\python
-
-  :: Go back to original current directory
+  :: go back to original current directory
   popd
