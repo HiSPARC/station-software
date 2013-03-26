99cfcffd0d458560dee524f4a6ce106c725e74f1
Robert Hart <robert@nikhef.nl>
2013-03-26 08:51:06 +0100

    Auto switch off screen saver when hisparc logs on.

--- persistent/startstopbatch/Startup.bat
+++ persistent/startstopbatch/Startup.bat
@@ -2,5 +2,7 @@
 
 if not "%USERNAME%" == "hisparc" exit
 
+@reg add "HKCU\Control Panel\Desktop" /v ScreenSaveActive /d 0 /f
+
 @call "%~dp0\..\..\user\startstop\runmanually.bat" \user\startstop StartUserMode.py
 @pause
