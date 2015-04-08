bf925e350c77d43eb9f678f2185a4646f8125a72
Arne de Laat <adelaat@nikhef.nl>
2015-04-08 12:01:39 +0200

    Update diagnostic tool message

--- user/diagnostictool/main.py
+++ user/diagnostictool/main.py
@@ -7,18 +7,18 @@ logger = logging.getLogger("diagnostics")
 
 help_message = """Welcome to the HiSPARC Diagnostic Tool.
 
-This tool is now performing some system checks and will display a summary at
-the end of this screen. If you have any problems with the VPN connection,
-please click on the `Write VPN config' button, reboot the computer and rerun
-this tool.
+This tool is now performing some system checks and will display a
+summary at the end of this screen. If you have any problems with the VPN
+connection, please click on the `Write VPN config' button, reboot the
+computer and rerun this tool.
 
-If you continue to have problems with any of these checks, please contact
-HiSPARC at info@hisparc.nl. It will be helpful if you include the contents of
-this screen (copy, paste should work for any decent mailer).
+If you continue to have problems with any of these checks, check the
+online documentation: http://docs.hisparc.nl/maintenance/ for possible
+causes and solutions.
 
-Please, refrain from embedding text or screenshots in Microsoft Word documents.
-Simply including text and graphics inside your message or as an attachment will
-enable us to more quickly assess the situation.
+If you need assistance please contact HiSPARC at beheer@hisparc.nl. It
+will be helpful if you include the contents of this screen (copy, paste
+should work for any decent mailer).
 
 Thank you!
 
@@ -44,6 +44,7 @@ def run_checks():
     logger.info(40 * '-')
     for check in enabled_checks:
         logger.info(str(check) + ': ' + status.string(check.status))
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
