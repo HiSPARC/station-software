344a2286e9579a67020dee4c40daf2bd3c49e584
Arne de Laat <adelaat@nikhef.nl>
2016-05-30 15:19:01 +0200

    Fix active check batch files

--- admin/nsclientpp/check_lvusage.bat
+++ admin/nsclientpp/check_lvusage.bat
@@ -1,2 +1,2 @@
 @echo off
-call "%~dp0..\..\user\startstop\runmanually.bat" \admin\nsclientpp active_check.py lvusage %1 %2
+call "%~dp0..\..\user\startstop\runmanually.bat" 0 \admin\nsclientpp active_check.py lvusage %1 %2
