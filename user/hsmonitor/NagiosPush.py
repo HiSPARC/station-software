25c6371ad106941a3f8a32828a1212f1574a1f06
Tom Kooij <tomkooij@tomkooij.nl>
2019-01-20 18:41:55 +0100

    Refactor NRDP JSON code for better readability

--- user/hsmonitor/NagiosPush.py
+++ user/hsmonitor/NagiosPush.py
@@ -20,10 +20,25 @@ class NagiosPush(object):
     def sendToNagios(self, nagiosResult):
         """HTTP POST service status to nagios server"""
 
+        nrdp_json = {
+            "checkresults": [
+                {
+                    "checkresult": {
+                        "type": "service",
+                        "checktype": "passive"
+                    },
+                    "hostname": self.machine_name,
+                    "servicename": nagiosResult.serviceName,
+                    "state": nagiosResult.status_code,
+                    "output": nagiosResult.description
+                }
+            ]
+        }
+
         params = {
            'token': NRDP_TOKEN,
            'cmd': 'submitcheck',
-           'JSONDATA': self._createNRDPJSON(nagiosResult),
+           'JSONDATA': json.dumps(nrdp_json),
         }
 
         headers = {
@@ -40,31 +55,6 @@ class NagiosPush(object):
                          % (nagiosResult.serviceName,
                             nagiosResult.status_code,
                             nagiosResult.description))
-
-    def _createNRDPJSON(self, nagiosResult):
-        """create JSON payload for NRDP passive checkresult"""
-
-        nrdp_json = {
-            "checkresults": [
-                {
-                    "checkresult": {
-                        "type": "service",
-                        "checktype": "passive"
-                    },
-                    "hostname": -1,
-                    "servicename": -1,
-                    "state": -1,
-                    "output": -1
-                }
-            ]
-        }
-
-        nrdp_json['checkresults'][0]['hostname'] = self.machine_name
-        nrdp_json['checkresults'][0]['servicename'] = nagiosResult.serviceName
-        nrdp_json['checkresults'][0]['state'] = nagiosResult.status_code
-        nrdp_json['checkresults'][0]['output'] = nagiosResult.description
-
-        return json.dumps(nrdp_json)
 
     def _getNRDPUrl(self, config):
         """get NRDP URL from config
