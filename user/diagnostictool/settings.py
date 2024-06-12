ffe1efca71effad247173710b247122198794dd6
Arne de Laat <arne@delaat.net>
2024-06-12 07:37:21 +0200

    Update upload url to utah

--- user/diagnostictool/settings.py
+++ user/diagnostictool/settings.py
@@ -1,3 +1,3 @@
-DATA_URL = 'http://frome.nikhef.nl/hisparc/upload'
+DATA_URL = 'http://hisparc-raw.chpc.utah.edu/hisparc/upload'
 
 ENABLED_CHECKS = ("data_connection", "vpn_connection", "proxy_settings")
