34279a4755cb8c82e22dc87c0f30182324a36d88
Arne de Laat <arne@delaat.net>
2024-06-12 07:36:55 +0200

    Remove code related to Nagios from station-software

--- user/hsmonitor/NagiosResult.py
+++
@@ -1,8 +0,0 @@
-"""Define the Nagios result structure."""
-
-
-class NagiosResult(object):
-    def __init__(self, status_code=None, description=None, serviceName=None):
-        self.status_code = status_code
-        self.description = description
-        self.serviceName = serviceName
